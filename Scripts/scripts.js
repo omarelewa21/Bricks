4291cf8c3613e5a56e8f47d527d6ac9df6e6db21
mahmoudnazih <mahmoud.n.zidan@gmail.com>
2018-12-11 12:29:31 +0400

    UPLOADING

---
+++ Scripts/scripts.js
@@ -0,0 +1,11 @@
+ function openNav() {
+     document.getElementById("myNav").style.width = "100%";
+ }
+ function openSearch() {
+     document.getElementById("search-nav").style.width = "100%";
+ }
+
+ function closeNav() {
+     document.getElementById("myNav").style.width = "0%";
+     document.getElementById("search-nav").style.width = "0%";
+ }
