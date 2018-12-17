0cde109ed0d441a2b88db97be17da6a26b52536c
mahmoudnazih <mahmoud.n.zidan@gmail.com>
2018-12-17 16:29:29 +0200

    mod

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,6 +1,7 @@
  function openNav() {
      document.getElementById("myNav").style.width = "100%";
  }
+
  function openSearch() {
      document.getElementById("search-nav").style.width = "100%";
  }
