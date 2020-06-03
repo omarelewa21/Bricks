35e4b56cf3518fa1f527acd8f79a781d16506811
mahmoudnazih <mahmoud.n.zidan@gmail.com>
2020-06-03 16:26:54 +0200

    ششش

--- template/short_nav.php
+++ template/short_nav.php
@@ -3,25 +3,43 @@
    <div class="container short-nav">
         <div class="row row-content">
         <div class="col-12">
-            <ul class="nav nav-tabs">
+            <ul class="nav nav-tabs tapped-nav">
                 <li class="nav-item">
-                    <a href="#hello" role="tab" data-toggle="tab" class="nav-link active">Hello</a>
+                    <a href="#on-sale" role="tab" data-toggle="tab" class="nav-link active">On Sale </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#hello1" role="tab" data-toggle="tab" class="nav-link ">Hello</a>
+                    <a href="#new-arrival" role="tab" data-toggle="tab" class="nav-link ">New Arrival</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#hello2" role="tab" data-toggle="tab" class="nav-link ">Hello</a>
+                    <a href="#top-categories" role="tab" data-toggle="tab" class="nav-link ">Top Categories</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#top-brands" role="tab" data-toggle="tab" class="nav-link ">Top Brands</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#best-sellers" role="tab" data-toggle="tab" class="nav-link ">Best Selllers</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#new-articles" role="tab" data-toggle="tab" class="nav-link ">New Articles</a>
                 </li>
             </ul>
             <div class="tab-content">
-                <div role="tabpanel" class="tab-pane fade show active" id="hello">
+                <div role="tabpanel" class="tab-pane fade show active" id="on-sale">
                     <p> Lorem Ipsum </p>
                 </div>
-                <div role="tabpanel" class="tab-pane fade  " id="hello1">
+                <div role="tabpanel" class="tab-pane fade  " id="new-arrival">
                     <p> Lorem  </p>
                 </div>
-                <div role="tabpanel" class="tab-pane fade  " id="hello2">
+                <div role="tabpanel" class="tab-pane fade  " id="top-categories">
+                    <p>  Ipsum </p>
+                </div>
+                <div role="tabpanel" class="tab-pane fade  " id="top-brands">
+                    <p>  Ipsum </p>
+                </div>
+                <div role="tabpanel" class="tab-pane fade  " id="best-sellers">
+                    <p>  Ipsum </p>
+                </div>
+                <div role="tabpanel" class="tab-pane fade  " id="new-articles">
                     <p>  Ipsum </p>
                 </div>
             </div>
